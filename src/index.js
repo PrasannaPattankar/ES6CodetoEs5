0289771a96b2c985f730db2d3f144ab49f4577d2
Prasanna <prasanna.pattankar@gmail.com>
2019-02-28 11:57:51 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let x = 10;
+let y = 20;
+if (x === 10) {
+  console.log(x);
+} else {
+  console.log(y);
+}
---
+++ src/script-compiled.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var x = 10;
+var y = 20;
+
+if (x === 10) {
+  console.log(x);
+} else {
+  console.log(y);
+}
