c7b44d6642eee52ee96072c5c09cf101a6e48a68
PrasannaPattankar <prasanna.pattankar@gmail.com>
2019-02-28 12:37:50 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-let x = 10;
-let y = 20;
-if (x === 10) {
-  console.log(x);
-} else {
-  console.log(y);
+function simpleExaml() {
+  let a = 10;
+  console.log("the value of first a is", a);
+  if (true) {
+    let a = 20;
+    console.log("the value of second a is", a);
+  }
+  console.log("the value of third a is", a);
 }
+simpleExaml();
+
+// same example  with keyword var
+
+function simpleExample() {
+  var b = 10;
+  console.log("the value of first b is", b);
+  if (true) {
+    var b = 20;
+    console.log("the value of second b is", b);
+  }
+  console.log("the value of  third b is", b);
+}
+simpleExample();
--- src/source-compiled.js
+++ src/source-compiled.js
@@ -1,10 +1,29 @@
 "use strict";
 
-var x = 10;
-var y = 20;
+function simpleExaml() {
+  var a = 10;
+  console.log("the value of first a is", a);
 
-if (x === 10) {
-  console.log(x);
-} else {
-  console.log(y);
+  if (true) {
+    var _a = 20;
+    console.log("the value of second a is", _a);
+  }
+
+  console.log("the value of third a is", a);
 }
+
+simpleExaml(); // same example  with keyword var
+
+function simpleExample() {
+  var b = 10;
+  console.log("the value of first b is", b);
+
+  if (true) {
+    var b = 20;
+    console.log("the value of second b is", b);
+  }
+
+  console.log("the value of  third b is", b);
+}
+
+simpleExample();
