5eb9708b76241e621246dfc760fd6fdd43181ebe
PrasannaPattankar <prasanna.pattankar@gmail.com>
2019-03-01 13:06:57 +0530

    second commit and third

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function simpleExaml() {
 }
 simpleExaml();
 
-// same example  with keyword var
+//same example  with keyword var
 
 function simpleExample() {
   var b = 10;
@@ -21,3 +21,32 @@ function simpleExample() {
   console.log("the value of  third b is", b);
 }
 simpleExample();
+
+// var d = 25;
+// console.log(d);
+// function simpleExamplee() {
+//   var c = 10;
+//   console.log("the value of first c is", c);
+//   if (true) {
+//     var c = 20;
+//     console.log("the value of second c is", c, d);
+//   }
+//   console.log("the value of  third c is", c);
+// }
+// console.log(d);
+// simpleExamplee();
+// var e = 24;
+// var e = 36;
+// console.log(e);
+
+// let f = 13;
+// let f = 36;
+// console.log(f);
+
+//console.log(adds(1, 9));
+//fat arrow function
+var add = (a, b) => a + b;
+console.log(add(1, 2));
+
+var mul = (a, b) => a * b;
+console.log(mul(3, 2));
--- src/source-compiled.js
+++ src/source-compiled.js
@@ -12,7 +12,7 @@ function simpleExaml() {
   console.log("the value of third a is", a);
 }
 
-simpleExaml(); // same example  with keyword var
+simpleExaml(); //same example  with keyword var
 
 function simpleExample() {
   var b = 10;
@@ -26,4 +26,35 @@ function simpleExample() {
   console.log("the value of  third b is", b);
 }
 
-simpleExample();
+simpleExample(); // var d = 25;
+// console.log(d);
+// function simpleExamplee() {
+//   var c = 10;
+//   console.log("the value of first c is", c);
+//   if (true) {
+//     var c = 20;
+//     console.log("the value of second c is", c, d);
+//   }
+//   console.log("the value of  third c is", c);
+// }
+// console.log(d);
+// simpleExamplee();
+// var e = 24;
+// var e = 36;
+// console.log(e);
+// let f = 13;
+// let f = 36;
+// console.log(f);
+//console.log(adds(1, 9));
+
+var add = function add(a, b) {
+  return a + b;
+};
+
+console.log(add(1, 2));
+
+var mul = function mul(a, b) {
+  return a * b;
+};
+
+console.log(mul(3, 2));
